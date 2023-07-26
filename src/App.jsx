168de1bb2f97bc0da87da1627db197e6e5e89514
arunabhnewar <newardevil@gmail.com>
2023-07-26 15:57:17 +0600

    build: react router dom & various empty page & components add

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,41 @@
-import Table from 'react-bootstrap/Table';
-import './App.css';
+import Table from "react-bootstrap/Table";
+import "./App.css";
 
 function App() {
-
   return (
-        <Table striped bordered hover>
-          <thead>
-            <tr>
-              <th>#</th>
-              <th>First Name</th>
-              <th>Last Name</th>
-              <th>Username</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>1</td>
-              <td>Mark</td>
-              <td>Otto</td>
-              <td>@mdo</td>
-            </tr>
-            <tr>
-              <td>2</td>
-              <td>Jacob</td>
-              <td>Thornton</td>
-              <td>@fat</td>
-            </tr>
-            <tr>
-              <td>3</td>
-              <td colSpan={2}>Larry the Bird</td>
-              <td>@twitter</td>
-            </tr>
-          </tbody>
-        </Table>
-      );
+    <>
+      <h1>Table</h1>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Username</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td colSpan={2}>Larry the Bird</td>
+            <td>@twitter</td>
+          </tr>
+        </tbody>
+      </Table>
+    </>
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/Dashboard/Sales/Sales.jsx
@@ -0,0 +1,9 @@
+const Sales = () => {
+  return (
+    <div>
+      <h1>Sales Page</h1>
+    </div>
+  );
+};
+
+export default Sales;
