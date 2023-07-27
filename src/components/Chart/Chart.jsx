25c9512ffffaec178a4ff8ac3f7762a654e7152c
arunabhnewar <newardevil@gmail.com>
2023-07-27 13:30:28 +0600

    feat: new 2 card add in sales dashboard page

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,53 +1,54 @@
 import {
-    Bar,
-    CartesianGrid,
-    Cell,
-    ComposedChart,
-    Line,
-    ResponsiveContainer,
-    Tooltip,
-    XAxis, YAxis
-} from 'recharts';
+  Bar,
+  CartesianGrid,
+  Cell,
+  ComposedChart,
+  Line,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
 
 const data = [
   {
-    name: 'Page A',
+    name: "Page A",
     uv: 4000,
     pv: 2400,
     amt: 2400,
   },
   {
-    name: 'Page B',
+    name: "Page B",
     uv: 3000,
     pv: 1398,
     amt: 2210,
   },
   {
-    name: 'Page C',
+    name: "Page C",
     uv: 2000,
     pv: 9800,
     amt: 2290,
   },
   {
-    name: 'Page D',
+    name: "Page D",
     uv: 2780,
     pv: 3908,
     amt: 2000,
   },
   {
-    name: 'Page E',
+    name: "Page E",
     uv: 1890,
     pv: 4800,
     amt: 2181,
   },
   {
-    name: 'Page F',
+    name: "Page F",
     uv: 2390,
     pv: 3800,
     amt: 2500,
   },
   {
-    name: 'Page G',
+    name: "Page G",
     uv: 3490,
     pv: 4300,
     amt: 2100,
@@ -56,40 +57,51 @@ const data = [
 
 const RevenueChart = () => {
   return (
-  <>
-  <h1 className='text-center mb-4'>Revenue Chart 2022-2023</h1>
-  
-    <ResponsiveContainer width="100%" height={400}>
-      <ComposedChart
-        data={data}
-        margin={{
-          top: 5,
-          right: 30,
-          left: 20,
-          bottom: 5,
-        }}
-      >
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="name" />
-        <YAxis yAxisId="left" />
-        <YAxis yAxisId="right" orientation="right" />
-        <Tooltip />
+    <>
+      <h1 className='text-center mb-4'>Revenue Chart 2023-2024</h1>
 
-        <Bar dataKey="uv" fill="#8884d8" yAxisId="left">
-          {data.map((entry, index) => (
-            <Cell key={`cell-${index}`} fill={'#2e75b5'} />
-          ))}
-        </Bar>
-        <Bar dataKey="pv" fill="#f6b26b" yAxisId="left">
-          {data.map((entry, index) => (
-            <Cell key={`cell-${index}`} fill={"#f6b26b"} />
-          ))}
-        </Bar>
+      <ResponsiveContainer width='100%' height={400}>
+        <ComposedChart
+          data={data}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}>
+          <CartesianGrid strokeDasharray='3 3' />
+          <XAxis dataKey='name' />
+          <YAxis yAxisId='left' />
+          <YAxis yAxisId='right' orientation='right' />
+          <Tooltip />
 
-        <Line type="monotone" dataKey="uv" stroke="#809499" strokeWidth={3} yAxisId="right" />
-        <Line type="monotone" dataKey="pv" stroke="#ffd966" strokeWidth={3} yAxisId="right" />
-      </ComposedChart>
-    </ResponsiveContainer>
+          <Bar dataKey='uv' fill='#8884d8' yAxisId='left'>
+            {data.map((entry, index) => (
+              <Cell key={`cell-${index}`} fill={"#2e75b5"} />
+            ))}
+          </Bar>
+          <Bar dataKey='pv' fill='#f6b26b' yAxisId='left'>
+            {data.map((entry, index) => (
+              <Cell key={`cell-${index}`} fill={"#f6b26b"} />
+            ))}
+          </Bar>
+
+          <Line
+            type='monotone'
+            dataKey='uv'
+            stroke='#809499'
+            strokeWidth={3}
+            yAxisId='right'
+          />
+          <Line
+            type='monotone'
+            dataKey='pv'
+            stroke='#ffd966'
+            strokeWidth={3}
+            yAxisId='right'
+          />
+        </ComposedChart>
+      </ResponsiveContainer>
     </>
   );
 };
--- src/pages/Dashboard/SalesDashboard/SalesDashboard.jsx
+++ src/pages/Dashboard/SalesDashboard/SalesDashboard.jsx
@@ -6,6 +6,7 @@ const SalesDashboard = () => {
     <>
       <PageTitle title='Sales Dashboard' />
       <div className='row'>
+        {/* Chart section */}
         <div className='col-12'>
           <div className='card'>
             <div className='card-body'>
@@ -69,7 +70,92 @@ const SalesDashboard = () => {
                   </div>
                 </div>
               </div> */}
-              <RevenueChart/>
+              <RevenueChart />
+            </div>
+          </div>
+        </div>
+        {/*  */}
+        <div className='col-xl-6 col-xxl-12 my-4'>
+          <div className='row'>
+            <div className='col-md-6'>
+              <div className='card progress-card'>
+                <div className='card-body d-flex'>
+                  <div className='me-auto'>
+                    <h4 className='card-title'>Total Transactions</h4>
+                    <div className='d-flex align-items-center'>
+                      <h2 className='fs-38 mb-0'>98k</h2>
+                      <div className='text-success transaction-caret'>
+                        <i className='fas fa-sort-up' />
+                        <p className='mb-0'>+0.5%</p>
+                      </div>
+                    </div>
+                  </div>
+                  {/* <div
+                    className='progress progress-vertical-bottom'
+                    style={{ minHeight: 110, minWidth: 10 }}>
+                    <div
+                      className='progress-bar bg-primary'
+                      style={{ width: 10, height: "40%" }}
+                      role='progressbar'>
+                      <span className='sr-only'>40% Complete</span>
+                    </div>
+                  </div>
+                  <div
+                    className='progress progress-vertical-bottom'
+                    style={{ minHeight: 110, minWidth: 10 }}>
+                    <div
+                      className='progress-bar bg-primary'
+                      style={{ width: 10, height: "55%" }}
+                      role='progressbar'>
+                      <span className='sr-only'>55% Complete</span>
+                    </div>
+                  </div>
+                  <div
+                    className='progress progress-vertical-bottom'
+                    style={{ minHeight: 110, minWidth: 10 }}>
+                    <div
+                      className='progress-bar bg-primary'
+                      style={{ width: 10, height: "80%" }}
+                      role='progressbar'>
+                      <span className='sr-only'>80% Complete</span>
+                    </div>
+                  </div>
+                  <div
+                    className='progress progress-vertical-bottom'
+                    style={{ minHeight: 110, minWidth: 10 }}>
+                    <div
+                      className='progress-bar bg-primary'
+                      style={{ width: 10, height: "50%" }}
+                      role='progressbar'>
+                      <span className='sr-only'>50% Complete</span>
+                    </div>
+                  </div> */}
+                </div>
+              </div>
+            </div>
+            <div className='col-md-6'>
+              <div className='card'>
+                <div className='card-body'>
+                  <h4 className='card-title'>Invoice Remaining</h4>
+                  {/* <div className='d-flex align-items-center'>
+                    <div className='me-auto'>
+                      <div className='progress mt-4' style={{ height: 10 }}>
+                        <div
+                          className='progress-bar bg-primary progress-animated'
+                          style={{ width: "45%", height: 10 }}
+                          role='progressbar'>
+                          <span className='sr-only'>60% Complete</span>
+                        </div>
+                      </div>
+                      <p className='fs-16 mb-0 mt-2'>
+                        <span className='text-danger'>-0,8% </span>from last
+                        month
+                      </p>
+                    </div>
+                    <h2 className='fs-38'>854</h2>
+                  </div> */}
+                </div>
+              </div>
             </div>
           </div>
         </div>
