6a118194d367b7666848330c516c49b2dc6b3ecf
JoySarkarBD <joysarkarbd407@gmail.com>
2023-07-28 09:40:39 +0600

    feat(components): add KPI & Metric

---
+++ src/components/Metric/MetricPO/MetricPO.jsx
@@ -0,0 +1,74 @@
+export default function MetricPO() {
+    return (
+       <>
+       
+       <h2 className="text-center mb-4">Metric-PO</h2>
+       <table className="table table-bordered">
+          <thead>
+            <tr>
+              <th scope="col">Department</th>
+              <th scope="col">Apr-23</th>
+              <th scope="col">May-23</th>
+              <th scope="col">Jun-23</th>
+              <th scope="col">Jul-23</th>
+              <th scope="col">Aug-23</th>
+              <th scope="col">Sep-23</th>
+              <th scope="col">Oct-23</th>
+              <th scope="col">Nov-23</th>
+              <th scope="col">Dec-23</th>
+              <th scope="col">Jan-24</th>
+              <th scope="col">Feb-24</th>
+              <th scope="col">Mar-24</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row">Department-1</th>
+              <td>Mark</td>
+              <td>Mark</td>
+              <td>Mark</td>
+              <td>Mark</td>
+              <td>Otto</td>
+              <td>Otto</td>
+              <td>Otto</td>
+              <td>Otto</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+            </tr>
+            <tr>
+              <th scope="row">Department-2</th>
+              <td>Jacob</td>
+              <td>Jacob</td>
+              <td>Jacob</td>
+              <td>Jacob</td>
+              <td>Thornton</td>
+              <td>Thornton</td>
+              <td>Thornton</td>
+              <td>Thornton</td>
+              <td>@fat</td>
+              <td>@fat</td>
+              <td>@fat</td>
+              <td>@fat</td>
+            </tr>
+            <tr>
+              <th scope="row">Department-3</th>
+              <td>@fat</td>
+              <td>@fat</td>
+              <td>@fat</td>
+              <td>@fat</td>
+              <td>Larry Bird</td>
+              <td>Larry Bird</td>
+              <td>Larry Bird</td>
+              <td>Larry Bird</td>
+              <td>@twitter</td>
+              <td>@twitter</td>
+              <td>@twitter</td>
+              <td>@twitter</td>
+            </tr>
+          </tbody>
+        </table>
+       </> 
+    )
+}
