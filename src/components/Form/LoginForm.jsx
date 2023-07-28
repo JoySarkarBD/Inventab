1473cae5628333f264978329fd3a6a90a0497744
MehediHasan23 <developer.mehedi23@gmail.com>
2023-07-28 17:17:31 +0600

    feat: login form functionality implemented

--- src/components/Form/LoginForm.jsx
+++ src/components/Form/LoginForm.jsx
@@ -1,3 +1,4 @@
+import { useFormik } from "formik";
 import { useState } from "react";
 import {
   // AiOutlineCamera,
@@ -5,6 +6,7 @@ import {
   AiOutlineEyeInvisible,
 } from "react-icons/ai";
 import { Link } from "react-router-dom";
+import axios from "../../utils/axios/axios";
 import ErrorMsg from "./ErrorMsg";
 import FormButton from "./FormButton";
 import TextInput from "./TextInput";
@@ -14,33 +16,65 @@ const LoginForm = () => {
   const togglePasswordVisibility = () => {
     setShowPassword(!showPassword);
   };
+
+  //login submition
+  const { values, handleChange, handleSubmit } = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    onSubmit: async (values) => {
+      try {
+        const { data } = await axios.post(
+          "accounts/login",
+          JSON.stringify(
+            { email: values.email, password: values.password },
+            {
+              headers: {
+                "Content-Type": "application/json",
+              },
+            }
+          )
+        );
+
+        console.log(data);
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  });
+
   return (
-    <form className="form">
+    <form className='form' onSubmit={handleSubmit}>
       {/* Email */}
-      <div className="mb-4">
+      <div className='mb-4'>
         <TextInput
-          type="email"
-          title="email"
-          placeholder="Enter your email"
-          defaultValue="hello@example.com"
+          type='email'
+          title='email'
+          name='email'
+          placeholder='Enter your email'
+          value={values.email}
+          onChange={handleChange}
         />
       </div>
       {/* Password */}
-      <div className="mb-4 ">
-        <div className="password_field">
+      <div className='mb-4 '>
+        <div className='password_field'>
           <TextInput
             type={`${showPassword ? "text" : "password"}`}
-            title="password"
-            placeholder="Enter your password"
-            defaultValue="Password"
+            title='password'
+            name='password'
+            placeholder='Enter your password'
+            value={values.password}
+            onChange={handleChange}
           />
-          <div onClick={togglePasswordVisibility} className="eyeIcon">
+          <div onClick={togglePasswordVisibility} className='eyeIcon'>
             {showPassword ? <AiOutlineEye /> : <AiOutlineEyeInvisible />}
           </div>
         </div>
-        <div className="mt-2 d-flex justify-content-between gap-4">
-          <ErrorMsg subject="Password not match" />
-          <Link to="forget-password">
+        <div className='mt-2 d-flex justify-content-between gap-4 d-none'>
+          <ErrorMsg subject='Password not match' />
+          <Link to='forget-password'>
             <p>Forget Password?</p>
           </Link>
         </div>
@@ -48,8 +82,8 @@ const LoginForm = () => {
       {/* show error msg */}
 
       {/* Login Button */}
-      <div className="mb-2">
-        <FormButton type="submit" title="Sign In" />
+      <div className='mb-2'>
+        <FormButton type='submit' title='Sign In' />
       </div>
     </form>
   );
--- src/components/Form/TextInput.jsx
+++ src/components/Form/TextInput.jsx
@@ -1,13 +1,9 @@
 // eslint-disable-next-line react/prop-types
-const TextInput = ({ title,...attributes }) => {
+const TextInput = ({ title, ...attributes }) => {
   return (
     <>
-      <label className="mb-2 text-dark text-capitalize">{title}</label>
-      <input
-        className="form-control form-control"
-        defaultValue="hello@example.com"
-        {...attributes}
-      />
+      <label className='mb-2 text-dark text-capitalize'>{title}</label>
+      <input className='form-control form-control' {...attributes} />
     </>
   );
 };
