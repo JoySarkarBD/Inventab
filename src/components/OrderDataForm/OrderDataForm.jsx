809a87988da90af58d1d3bba49089d9b37ea3d63
arunabhnewar <newardevil@gmail.com>
2023-07-31 14:05:44 +0600

    fix: order update select fix

--- src/components/OrderDataForm/OrderDataForm.jsx
+++ src/components/OrderDataForm/OrderDataForm.jsx
@@ -12,11 +12,11 @@ export default function DataForm() {
     const obj = {
       id: v4(),
     };
-    setTableLength((prev) => [...prev, obj]);
+    setTableLength(prev => [...prev, obj]);
   };
 
-  const handleRemove = (id) => {
-    const modifiedArray = tableLength.filter((table) => table.id !== id);
+  const handleRemove = id => {
+    const modifiedArray = tableLength.filter(table => table.id !== id);
     setTableLength(modifiedArray);
   };
 
@@ -30,170 +30,170 @@ export default function DataForm() {
   ];
 
   return (
-    <div className="card-body">
+    <div className='card-body'>
       <form>
-        <div className="row">
+        <div className='row'>
           {/* Sales Lead input */}
-          <div className="mb-3 col-md-6">
-            <label className="mb-2 text-dark text-capitalize">Sales Lead</label>
-            <Select title="Sales Lead" placeholder="Sales Lead" />
+          <div className='mb-3 col-md-6'>
+            <label className='mb-2 text-dark text-capitalize'>Sales Lead</label>
+            <Select title='Sales Lead' placeholder='Sales Lead' />
           </div>
 
           {/* add sub org input */}
-          <div className="mb-3 col-md-6">
-            <label className="mb-2 text-dark text-capitalize">Sub org</label>
-            <Select placeholder="Select Sub Org" />
+          <div className='mb-3 col-md-6'>
+            <label className='mb-2 text-dark text-capitalize'>Sub org</label>
+            <Select placeholder='Select Sub Org' />
           </div>
 
           {/* Rof PO NO input */}
-          <div className="mb-3 col-md-6">
-            <InputText title="Rof PO No" type="text" />
+          <div className='mb-3 col-md-6'>
+            <InputText title='Rof PO No' type='text' />
           </div>
 
           {/* Rof PO Date input */}
-          <div className="mb-3 col-md-6">
-            <InputText title="Rof PO Date" type="date" />
+          <div className='mb-3 col-md-6'>
+            <InputText title='Rof PO Date' type='date' />
           </div>
 
           {/* add invoice date input */}
-          <div className="mb-3 col-md-6">
-            <InputText title="Expected Invoice Date*" type="date" />
+          <div className='mb-3 col-md-6'>
+            <InputText title='Expected Invoice Date*' type='date' />
           </div>
 
           {/* Select Payment Term */}
-          <div className="mb-3 col-md-6">
-            <label className="mb-2 text-dark text-capitalize">
+          <div className='mb-3 col-md-6'>
+            <label className='mb-2 text-dark text-capitalize'>
               Select Payment Term
             </label>
-            <Select placeholder="Select Payment Term" />
+            <Select placeholder='Select Payment Term' />
           </div>
 
           {/* Select Delivery Term */}
-          <div className="mb-3 col-md-6">
-            <label className="mb-2 text-dark text-capitalize">
+          <div className='mb-3 col-md-6'>
+            <label className='mb-2 text-dark text-capitalize'>
               Select Delivery Term
             </label>
-            <Select placeholder="Select Delivery Term" />
+            <Select placeholder='Select Delivery Term' />
           </div>
 
           {/* Select Client */}
-          <div className="mb-3 col-md-6">
-            <label className="mb-2 text-dark text-capitalize">
+          <div className='mb-3 col-md-6'>
+            <label className='mb-2 text-dark text-capitalize'>
               Select Client
             </label>
-            <Select placeholder="Select Client" />
+            <Select placeholder='Select Client' />
           </div>
 
           {/* Select Client Contact */}
-          <div className="mb-3 col-md-6">
-            <label className="mb-2 text-dark text-capitalize">
+          <div className='mb-3 col-md-6'>
+            <label className='mb-2 text-dark text-capitalize'>
               Select Client Contact
             </label>
-            <Select placeholder="Select Client Contact" />
+            <Select placeholder='Select Client Contact' />
           </div>
 
           {/* Select Billing Address */}
-          <div className="mb-3 col-md-6">
-            <label className="mb-2 text-dark text-capitalize">
+          <div className='mb-3 col-md-6'>
+            <label className='mb-2 text-dark text-capitalize'>
               Select Billing Address
             </label>
-            <Select placeholder="Select Billing Address" />
+            <Select placeholder='Select Billing Address' />
           </div>
 
           {/* Select Shipping Address */}
-          <div className="mb-3 col-md-6">
-            <label className="mb-2 text-dark text-capitalize">
+          <div className='mb-3 col-md-6'>
+            <label className='mb-2 text-dark text-capitalize'>
               Select Shipping Address
             </label>
-            <Select placeholder="Select Shipping Address" />
+            <Select placeholder='Select Shipping Address' />
           </div>
 
           {/* description input */}
-          <div className="mb-3 col-md-6">
+          <div className='mb-3 col-md-6'>
             <TextArea
-              title="Description*"
-              className="w-100"
-              placeholder="Description"
+              title='Description*'
+              className='w-100'
+              placeholder='Description'
             />
             <br />
           </div>
 
           {/* Comment input */}
-          <div className="mb-3 col-12">
+          <div className='mb-3 col-12'>
             <TextArea
-              title="Comment*"
-              className="w-100"
-              placeholder="Comment"
+              title='Comment*'
+              className='w-100'
+              placeholder='Comment'
             />
             <br />
           </div>
         </div>
         {/* Table Part */}
         {/* Table Row Add Button */}
-        <div className="d-flex justify-content-end my-4">
-          <button className="btn btn-primary rounded" onClick={handleTable}>
+        <div className='d-flex justify-content-end my-4'>
+          <button className='btn btn-primary rounded' onClick={handleTable}>
             Add Table Row
           </button>
         </div>
         {/* Table */}
-        <div className="table-responsive">
-          <table className="table table-bordered table-responsive">
+        <div className='table-responsive'>
+          <table className='table table-bordered table-responsive'>
             <thead>
               <tr>
-                <th scope="col">Port No</th>
-                <th scope="col">Unit Cost</th>
-                <th scope="col">Total Quantity</th>
-                <th scope="col">Extd Net Cost</th>
-                <th scope="col">Extd Gross Cost</th>
-                <th scope="col">Action</th>
+                <th scope='col'>Port No</th>
+                <th scope='col'>Unit Cost</th>
+                <th scope='col'>Total Quantity</th>
+                <th scope='col'>Extd Net Cost</th>
+                <th scope='col'>Extd Gross Cost</th>
+                <th scope='col'>Action</th>
               </tr>
             </thead>
             <tbody>
-              {tableLength.map((table) => {
+              {tableLength.map(table => {
                 return (
                   <tr key={table.id}>
                     <td>
-                      <div className="select-port">
+                      <div className='select-port'>
                         <Select
-                          className="select"
+                          className='select'
                           options={options}
-                          placeholder="Select Port No"
+                          placeholder='Select Port No'
+                          menuPortalTarget={document.querySelector("body")}
                         />
                       </div>
                     </td>
                     <td>
                       <input
-                        className="new_input_class"
-                        type="number"
-                        placeholder="Unit Cost"
+                        className='new_input_class'
+                        type='number'
+                        placeholder='Unit Cost'
                       />
                     </td>
                     <td>
                       <input
-                        className="new_input_class"
-                        type="number"
-                        placeholder="Total Quntity"
+                        className='new_input_class'
+                        type='number'
+                        placeholder='Total Quntity'
                       />
                     </td>
                     <td>
                       <input
-                        className="new_input_class"
-                        type="number"
-                        placeholder="Extd Net Cost"
+                        className='new_input_class'
+                        type='number'
+                        placeholder='Extd Net Cost'
                       />
                     </td>
                     <td>
                       <input
-                        className="new_input_class"
-                        type="number"
-                        placeholder="Extd Gross Cost"
+                        className='new_input_class'
+                        type='number'
+                        placeholder='Extd Gross Cost'
                       />
                     </td>
                     <td>
                       <button
-                        className="btn btn-danger btn-sm"
-                        onClick={() => handleRemove(table.id)}
-                      >
+                        className='btn btn-danger btn-sm'
+                        onClick={() => handleRemove(table.id)}>
                         Remove
                       </button>
                     </td>
@@ -204,11 +204,11 @@ export default function DataForm() {
           </table>
         </div>
         {/* Submit Button */}
-        <div className="d-flex justify-content-end my-4">
+        <div className='d-flex justify-content-end my-4'>
           <input
-            className="btn btn-primary"
-            type="submit"
-            value="Add Sales Lead"
+            className='btn btn-primary'
+            type='submit'
+            value='Add Sales Lead'
           />
         </div>
       </form>
