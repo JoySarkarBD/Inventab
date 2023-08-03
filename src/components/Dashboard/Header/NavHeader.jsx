e1f6afa588d135ad38dbd8710278dfe3c2c975ef
diptowebhero <diptodas1672@gmail.com>
2023-08-03 20:09:25 +0600

    fix: clean css

--- src/components/Dashboard/Header/NavHeader.jsx
+++ src/components/Dashboard/Header/NavHeader.jsx
@@ -3,17 +3,17 @@ import tabLogo from "../../../assets/images/tab-logo.png";
 const NavHeader = () => {
   return (
     <>
-      <div className='nav-header'>
+      <div className="nav-header">
         {/* logo */}
-        <a href='#' className='brand-logo'>
-          <img src={tabLogo} className='logo-abbr' alt='logo' width='95px' />
+        <a href="#" className="brand-logo">
+          <img src={tabLogo} className="logo-abbr" alt="logo" width="95px" />
         </a>
         {/* burger menu */}
-        <div className='nav-control'>
-          <div className='hamburger'>
-            <span className='line'></span>
-            <span className='line'></span>
-            <span className='line'></span>
+        <div className="nav-control">
+          <div className="hamburger">
+            <span className="line"></span>
+            <span className="line"></span>
+            <span className="line"></span>
           </div>
         </div>
       </div>
