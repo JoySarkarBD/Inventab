5d7cfd1220b9be866a0bc283cc66cb14172b50aa
diptowebhero <diptodas1672@gmail.com>
2023-08-04 10:20:13 +0600

    fix: clean css

--- src/utils/axios/axios.js
+++ src/utils/axios/axios.js
@@ -3,6 +3,6 @@ export default axios.create({
   baseURL: "http://inventab.io/api/v1/",
   headers: {
     "Content-Type": "application/json",
-    Authorization: `JWT eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjkxMDk3NjIyLCJqdGkiOiI1YTVhY2Y2YTlhNTI0YjU5YTY5MzU5ZDM2ZDQ3NmUwNiIsInVzZXJfaWQiOiI4N2NmNTQ2My04YjNiLTQwZmMtOWFlZS01ZDdkYzk2Y2EzMmQifQ.-vrFb8-sh3H3SCzOrV_jbFshQFsdCygy14ROL3Kf6wg`,
+    Authorization: `JWT eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjkxMTQzODM4LCJqdGkiOiJjNjNlZDY2NDkwMTk0OTgzYWY2NTY4ZWQ4MjJiZDRmMCIsInVzZXJfaWQiOiI4N2NmNTQ2My04YjNiLTQwZmMtOWFlZS01ZDdkYzk2Y2EzMmQifQ.jkglTPTgn3N2Y3CR7ieL2i4lTDQ2DWdIMN__C3JXSnw`,
   },
 });
