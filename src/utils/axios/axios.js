ecf2e17d355cb08667ced313e956622bd5ef1e9a
JoySarkarBD <joysarkarbd407@gmail.com>
2023-08-04 20:20:12 +0600

    fix: error

--- src/utils/axios/axios.js
+++ src/utils/axios/axios.js
@@ -3,6 +3,6 @@ export default axios.create({
   baseURL: "http://inventab.io/api/v1/",
   headers: {
     "Content-Type": "application/json",
-    Authorization: `JWT eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjkxMTcyOTEwLCJqdGkiOiJkYzIxOWU5YzI3YzQ0YWVjOGEyZTlkYTg3MWZlOTY5NSIsInVzZXJfaWQiOiJjMGE2ZmE2MC1hNWFmLTQzYWYtOTE4Yi05OTNlZjAxYmQ1NmUifQ.RJ64KtD0CwgFVrbTDxrpYB0hQMmXnqVrgyAoOatoVxY`,
+    Authorization: `JWT eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjkxMTc3MzY2LCJqdGkiOiJlNmU1ZDFjNmQzNGE0MDUwOGMyNWNiOGRiZmFjYTZkNyIsInVzZXJfaWQiOiJjMGE2ZmE2MC1hNWFmLTQzYWYtOTE4Yi05OTNlZjAxYmQ1NmUifQ.lhsSv3o2YcOTKfu370rcYMTEItQJOd1L7VdOvbIzZNI`,
   },
 });
