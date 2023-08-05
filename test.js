d668ccb8990128dc69850b471e8a5a23d103a31a
JoySarkarBD <joysarkarbd407@gmail.com>
2023-08-05 22:40:33 +0600

    env config

--- test.js
+++ test.js
@@ -9,3 +9,13 @@ for (let i of arr) {
 }
 
 console.log(total);
+
+/* 
+
+ORG_ID = 0a055b26-ae15-40a9-8291-25427b94ebb3
+FIRST_NAME = Test
+LAST_NAME = Test
+USER_ID = 87cf5463-8b3b-40fc-9aee-5d7dc96ca32d
+EMAIL = test.user@gmail.com
+
+*/
