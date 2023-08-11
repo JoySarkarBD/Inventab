6a9ac41b838c763c5e000ac6de180ac58d81efb3
JoySarkarBD <joysarkarbd407@gmail.com>
2023-08-11 18:07:16 +0600

    file: remove some files

--- src/context/authContext.jsx
+++ src/context/authContext.jsx
@@ -4,7 +4,7 @@ import { createContext, useState } from "react";
 export const AuthContext = createContext(null);
 
 const AuthContextProvider = ({ children }) => {
-  const prevOrg = null; /* || `0a055b26-ae15-40a9-8291-25427b94ebb3` */
+  const prevOrg = null || `0a055b26-ae15-40a9-8291-25427b94ebb3`;
   const [auth, setAuth] = useState({
     accessToken: `eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjkxNzczODg2LCJqdGkiOiI5ZWNmMjkyOWMzMjk0ZGM2YmNkZGE3ODkyNGFmNDg1MCIsInVzZXJfaWQiOiIyYWYwZTcwMC00NGE5LTQ0OWUtYjg1MS0xMDU1MDRlZjdmYTIifQ.ACu686g9BpXPapGfMs79OfcG3xPJtNmCij0DxMUwPcI`,
     firstname: `Ura1`,
