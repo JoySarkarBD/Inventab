c4f68aa4ba20f0c14522452893c63397aed9f9ce
MehediHasan23 <developer.mehedi23@gmail.com>
2023-08-12 13:50:14 +0600

    feat: auth system done

--- src/pages/Dashboard/SalesDashboard/SalesDashboard.jsx
+++ src/pages/Dashboard/SalesDashboard/SalesDashboard.jsx
@@ -1,4 +1,3 @@
-import { Toaster } from "react-hot-toast";
 import RevenueChart from "../../../components/Chart/Chart";
 import KPIInvoice from "../../../components/KPI/KPI-Invoice/KPIInvoice";
 import KPIPO from "../../../components/KPI/KPI-PO/KPIPO";
@@ -12,7 +11,7 @@ import "./SalesDashboard.css";
 const SalesDashboard = () => {
   return (
     <>
-      <Toaster />
+      {/* <Toaster /> */}
       <PageTitle title='Sales Dashboard' />
       <div className='row'>
         {/* KPI table */}
