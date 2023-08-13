f5cac86a5d49586baf7f1b467b680c4251a1da71
MehediHasan23 <developer.mehedi23@gmail.com>
2023-08-13 13:28:18 +0600

    fix: serilized no in sales invoice

--- src/pages/Dashboard/Sales/SalesInvoiceDetails.jsx
+++ src/pages/Dashboard/Sales/SalesInvoiceDetails.jsx
@@ -9,7 +9,196 @@ import SectionTitle from "../../../components/Shared/SectionTitle";
 import useAxiosPrivate from "../../../hooks/useAxiosPrivate";
 import "./sales.css";
 
+const data = {
+  id: "fcc644c1-9579-42d7-8f94-d6744ce8e72a",
+  org: {
+    id: "64cdfb09-1b78-4bc6-8d9f-c7cd2b38f50a",
+  },
+  sale_order: "SO-100000194-000025-(1831123030)",
+  parts_invoice: [
+    {
+      id: "14a8b278-cbc2-432a-b4b8-6814a1d83eaf",
+      parts_no: {
+        id: "55032561-c0bc-40ec-bfeb-ee3f410b0271",
+        part_type: {
+          id: 3,
+          created: "2021-08-10T07:30:09.466454+05:30",
+          modified: "2021-10-07T15:33:23.967533+05:30",
+          name: "Product",
+        },
+        gst_itm: {
+          id: "f80e93a9-1605-4434-a356-f005df6f873d",
+          hsn_or_sac: "90318000",
+          description:
+            "MEASURING OR CHECKING INSTRUMENTS, APPLIANCES AND MACHINES, NOT SPECIFIED OR INCLUDED ELSEWHERE IN THIS CHAPTER; PROFILE PROJECTORS OTHER INSTRUMENTS, APPLIANCES AND MACHINES",
+        },
+        serialized_parts: [
+          {
+            id: "af13ef5f-117b-4ca8-9d4d-6b92b9bfa340",
+            serial_number: "08:3A:F2:15:CC:24",
+          },
+          {
+            id: "af13ef5f-117b-4ca8-9d4d-6b92b9bfa370",
+            serial_number: "08:3A:F2:15:CC:26",
+          },
+          {
+            id: "af13ef5f-117b-4ca8-9d4d-6b92b9bfa390",
+            serial_number: "08:3A:F2:15:CC:28",
+          },
+        ],
+        created: "2022-07-14T18:45:35.828393+05:30",
+        modified: "2023-07-09T09:49:30.997513+05:30",
+        internal_part_no: "PN-INV-0000000210",
+        part_number: "ST000070",
+        customer_part_number: "ST000070",
+        bom: false,
+        short_description: "ST000070 - DA Lite WIFI Dongle Auto (Pink)",
+        long_description: "ST000070 - DA Lite WIFI Dongle Auto (Pink)",
+        mrp: 8500.0,
+        weight: "0.08",
+        length: "100.00",
+        breadth: "60.00",
+        height: "30.00",
+        serialization: true,
+        is_active: true,
+        warranty_period: 12,
+        warranty_terms: "12 MONTH",
+        packing_charge: 0.0,
+        manufacturer: "e9524f74-e70e-4aee-aeb1-56825d8e23ee",
+        part_category: 5,
+        sub_category: 13,
+      },
+      created: "2023-07-07T17:33:54.951442+05:30",
+      modified: "2023-07-07T17:33:54.951476+05:30",
+      quantity: 150,
+      customer_part_no: "ST000070",
+      price: 8500.0,
+      warranty: 12,
+      short_description: "ST000070 - DA Lite WIFI Dongle Auto (Pink)",
+      invoice: "fcc644c1-9579-42d7-8f94-d6744ce8e72a",
+      part: "249c3d04-5b5e-4eae-978d-33374e895bec",
+    },
+    {
+      id: "14a8b278-cbc2-432a-b4b8-6814a1d83eag",
+      parts_no: {
+        id: "55032561-c0bc-40ec-bfeb-ee3f410b0271",
+        part_type: {
+          id: 3,
+          created: "2021-08-10T07:30:09.466454+05:30",
+          modified: "2021-10-07T15:33:23.967533+05:30",
+          name: "Product",
+        },
+        gst_itm: {
+          id: "f80e93a9-1605-4434-a356-f005df6f873d",
+          hsn_or_sac: "90318000",
+          description:
+            "MEASURING OR CHECKING INSTRUMENTS, APPLIANCES AND MACHINES, NOT SPECIFIED OR INCLUDED ELSEWHERE IN THIS CHAPTER; PROFILE PROJECTORS OTHER INSTRUMENTS, APPLIANCES AND MACHINES",
+        },
+        serialized_parts: [
+          {
+            id: "af13ef5f-117b-4ca8-9d4d-6b92b9bfa341",
+            serial_number: "08:3A:F2:15:CC:31",
+          },
+          {
+            id: "af13ef5f-117b-4ca8-9d4d-6b92b9bfa342",
+            serial_number: "08:3A:F2:15:CC:25",
+          },
+        ],
+        created: "2022-07-14T18:45:35.828393+05:30",
+        modified: "2023-07-09T09:49:30.997513+05:30",
+        internal_part_no: "PN-INV-0000000210",
+        part_number: "ST000012",
+        customer_part_number: "ST000012",
+        bom: false,
+        short_description: "ST00012 - DA Lite WIFI Dongle Auto (Yellow)",
+        long_description: "ST00012 - DA Lite WIFI Dongle Auto (Yellow)",
+        mrp: 85300.0,
+        weight: "0.08",
+        length: "100.00",
+        breadth: "60.00",
+        height: "30.00",
+        serialization: true,
+        is_active: true,
+        warranty_period: 12,
+        warranty_terms: "12 MONTH",
+        packing_charge: 0.0,
+        manufacturer: "e9524f74-e70e-4aee-aeb1-56825d8e23ee",
+        part_category: 5,
+        sub_category: 13,
+      },
+      created: "2023-07-07T17:33:54.951442+05:30",
+      modified: "2023-07-07T17:33:54.951476+05:30",
+      quantity: 150,
+      customer_part_no: "ST000012",
+      price: 500.0,
+      warranty: 12,
+      short_description: "ST00012 - DA Lite WIFI Dongle Auto (Yellow)",
+      invoice: "fcc644c1-9579-42d7-8f94-d6744ce8e72a",
+      part: "249c3d04-5b5e-4eae-978d-33374e895becaaa",
+    },
+    {
+      id: "14a8b278-cbc2-432a-b4b8-6814a1d83eaw",
+      parts_no: {
+        id: "55032561-c0bc-40ec-bfeb-ee3f410b0271",
+        part_type: {
+          id: 3,
+          created: "2021-08-10T07:30:09.466454+05:30",
+          modified: "2021-10-07T15:33:23.967533+05:30",
+          name: "Product",
+        },
+        gst_itm: {
+          id: "f80e93a9-1605-4434-a356-f005df6f873d",
+          hsn_or_sac: "90318000",
+          description:
+            "MEASURING OR CHECKING INSTRUMENTS, APPLIANCES AND MACHINES, NOT SPECIFIED OR INCLUDED ELSEWHERE IN THIS CHAPTER; PROFILE PROJECTORS OTHER INSTRUMENTS, APPLIANCES AND MACHINES",
+        },
+        serialized_parts: [
+          {
+            id: "af13ef5f-117b-4ca8-9d4d-6b92b9bfa344",
+            serial_number: "08:3A:F2:15:CC:35",
+          },
+          {
+            id: "af13ef5f-117b-4ca8-9d4d-6b92b9bfa347",
+            serial_number: "08:3A:F2:15:CC:38",
+          },
+        ],
+        created: "2022-07-14T18:45:35.828393+05:30",
+        modified: "2023-07-09T09:49:30.997513+05:30",
+        internal_part_no: "PN-INV-0000000210",
+        part_number: "ST000090",
+        customer_part_number: "ST000090",
+        bom: false,
+        short_description: "ST000090- DA Lite WIFI Dongle Auto (Red)",
+        long_description: "ST000090- DA Lite WIFI Dongle Auto (Red)",
+        mrp: 8500.0,
+        weight: "0.08",
+        length: "100.00",
+        breadth: "60.00",
+        height: "30.00",
+        serialization: true,
+        is_active: true,
+        warranty_period: 12,
+        warranty_terms: "12 MONTH",
+        packing_charge: 0.0,
+        manufacturer: "e9524f74-e70e-4aee-aeb1-56825d8e23ees",
+        part_category: 5,
+        sub_category: 13,
+      },
+      created: "2023-07-07T17:33:54.951442+05:30",
+      modified: "2023-07-07T17:33:54.951476+05:30",
+      quantity: 40,
+      customer_part_no: "ST000090",
+      price: 800.0,
+      warranty: 12,
+      short_description: "ST000090- DA Lite WIFI Dongle Auto (Red)",
+      invoice: "fcc644c1-9579-42d7-8f94-d6744ce8e72a",
+      part: "249c3d04-5b5e-4eae-978d-33374e895bec",
+    },
+  ],
+};
+
 const SalesInvoiceDetails = () => {
+  const [serializedNo, setSerializedNo] = useState("");
   const { invoice_id } = useParams();
   const axios = useAxiosPrivate();
 
@@ -42,6 +231,8 @@ const SalesInvoiceDetails = () => {
       (isMount = false), controller.abort();
     };
   }, [invoice_id, axios]);
+
+  console.log(serializedNo);
 
   return (
     <div>
@@ -241,7 +432,7 @@ const SalesInvoiceDetails = () => {
                     </tr>
                   </thead>
                   <tbody>
-                    {invoiceDetails?.parts_invoice?.map((part) => {
+                    {data?.parts_invoice?.map((part) => {
                       return (
                         <tr key={part?.id}>
                           <td>
@@ -251,7 +442,8 @@ const SalesInvoiceDetails = () => {
                               className='btn btn-primary btn-sm'
                               type='button'
                               data-bs-toggle='modal'
-                              data-bs-target='#exampleModal'>
+                              data-bs-target='#exampleModal'
+                              onClick={() => setSerializedNo(part.id)}>
                               Serial no
                             </button>
                             {/* mrp in unit value */}
@@ -326,23 +518,25 @@ const SalesInvoiceDetails = () => {
                   </tr>
                 </thead>
                 <tbody>
-                  {invoiceDetails?.parts_invoice?.map((part) => {
-                    return (
-                      <tr key={part?.id}>
-                        <td>
-                          {part?.parts_no?.serialized_parts?.map(
-                            (sn, index) => {
-                              return <p key={sn?.id}>{"Sl-" + ++index}</p>;
-                            }
-                          )}
-                        </td>
-                        <td>
-                          {part?.parts_no?.serialized_parts?.map((sn) => {
-                            return <p key={sn?.id}>{sn?.serial_number}</p>;
-                          })}
-                        </td>
-                      </tr>
-                    );
+                  {data?.parts_invoice?.map((part) => {
+                    if (part?.id === serializedNo) {
+                      return (
+                        <tr key={part?.id}>
+                          <td>
+                            {part?.parts_no?.serialized_parts?.map(
+                              (sn, index) => {
+                                return <p key={sn?.id}>{"Sl-" + ++index}</p>;
+                              }
+                            )}
+                          </td>
+                          <td>
+                            {part?.parts_no?.serialized_parts?.map((sn) => {
+                              return <p key={sn?.id}>{sn?.serial_number}</p>;
+                            })}
+                          </td>
+                        </tr>
+                      );
+                    }
                   })}
                 </tbody>
               </table>
