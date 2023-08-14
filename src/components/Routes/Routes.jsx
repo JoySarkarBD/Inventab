88636754c35007e60f2aa5bf07a5cb1fc2be5a88
arunabhnewar <newardevil@gmail.com>
2023-08-14 15:06:02 +0600

    refactor: auth context & router change

--- src/components/Routes/Routes.jsx
+++ src/components/Routes/Routes.jsx
@@ -1,3 +1,105 @@
+// import { createBrowserRouter } from "react-router-dom";
+// import DashboardLayout from "../../Layout/DashboardLayout";
+// import MainLayout from "../../Layout/MainLayout";
+// import AR from "../../pages/Dashboard/Sales/AR";
+// import SalesInvoiceDetails from "../../pages/Dashboard/Sales/SalesInvoiceDetails";
+// import SalesInvoices from "../../pages/Dashboard/Sales/SalesInvoices";
+// import SalesLead from "../../pages/Dashboard/Sales/SalesLead";
+// import SalesOrders from "../../pages/Dashboard/Sales/SalesOrders";
+// import SalesDashboard from "../../pages/Dashboard/SalesDashboard/SalesDashboard";
+// import Attendance from "../../pages/Dashboard/Support/Attendance";
+// import LogIn from "../../pages/Login/LogIn";
+// import AddSalesLeads from "../../pages/SalesLeads/AddSalesLeads";
+// import UpdateSalesLeads from "../../pages/SalesLeads/UpdateSalesLeads";
+// import AddSalesOrder from "../../pages/SalesOrder/AddSalesOrder";
+// import UpdateSalesOrder from "../../pages/SalesOrder/UpdateSalesOrder";
+// import ErrorPage from "../../ui/ErrorPage";
+// import AuthRequired from "../AuthRequired/AuthRequired";
+// import PublicRoute from "../PublicRoute/PublicRoute";
+
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <MainLayout />,
+//     errorElement: <ErrorPage />,
+//     children: [
+//       {
+//         element: <PublicRoute />,
+//         children: [
+//           {
+//             path: "/",
+//             element: <LogIn />,
+//           },
+//         ],
+//       },
+//     ],
+//   },
+//   {
+//     element: <AuthRequired />,
+//     children: [
+//       {
+//         path: "/dashboard",
+//         element: <DashboardLayout />,
+//         errorElement: <ErrorPage />,
+//         children: [
+//           {
+//             path: "/dashboard",
+//             element: <SalesDashboard />,
+//           },
+//           /* Leads */
+//           {
+//             path: "sales-leads",
+//             element: <SalesLead />,
+//           },
+//           {
+//             path: "sales/add-sales-leads",
+//             element: <AddSalesLeads />,
+//           },
+//           {
+//             path: "sales/update-sales-leads/:lead_no",
+//             element: <UpdateSalesLeads />,
+//           },
+//           /* Orders */
+//           {
+//             path: "sales-orders",
+//             element: <SalesOrders />,
+//           },
+//           {
+//             path: "sales-orders/add-sales-order",
+//             element: <AddSalesOrder />,
+//           },
+
+//           {
+//             path: "sales-orders/update-sales-order/:order_id",
+//             element: <UpdateSalesOrder />,
+//           },
+//           /* invoice */
+//           {
+//             path: "sales-invoices",
+//             element: <SalesInvoices />,
+//           },
+//           {
+//             path: "sales-invoices/sales-invoices-details/:invoice_id",
+//             element: <SalesInvoiceDetails />,
+//           },
+//           /* ar */
+//           {
+//             path: "ar",
+//             element: <AR />,
+//           },
+//           /* attendance */
+//           {
+//             path: "attendance",
+//             element: <Attendance />,
+//           },
+//         ],
+//       },
+//     ],
+//   },
+// ]);
+
+// export default router;
+
 import { createBrowserRouter } from "react-router-dom";
 import DashboardLayout from "../../Layout/DashboardLayout";
 import MainLayout from "../../Layout/MainLayout";
@@ -14,8 +116,6 @@ import UpdateSalesLeads from "../../pages/SalesLeads/UpdateSalesLeads";
 import AddSalesOrder from "../../pages/SalesOrder/AddSalesOrder";
 import UpdateSalesOrder from "../../pages/SalesOrder/UpdateSalesOrder";
 import ErrorPage from "../../ui/ErrorPage";
-import AuthRequired from "../AuthRequired/AuthRequired";
-import PublicRoute from "../PublicRoute/PublicRoute";
 
 const router = createBrowserRouter([
   {
@@ -24,75 +124,65 @@ const router = createBrowserRouter([
     errorElement: <ErrorPage />,
     children: [
       {
-        element: <PublicRoute />,
-        children: [
-          {
-            path: "/",
-            element: <LogIn />,
-          },
-        ],
+        path: "/",
+        element: <LogIn />,
       },
     ],
   },
   {
-    element: <AuthRequired />,
+    path: "/dashboard",
+    element: <DashboardLayout />,
+    errorElement: <ErrorPage />,
     children: [
       {
         path: "/dashboard",
-        element: <DashboardLayout />,
-        errorElement: <ErrorPage />,
-        children: [
-          {
-            path: "/dashboard",
-            element: <SalesDashboard />,
-          },
-          /* Leads */
-          {
-            path: "sales-leads",
-            element: <SalesLead />,
-          },
-          {
-            path: "sales/add-sales-leads",
-            element: <AddSalesLeads />,
-          },
-          {
-            path: "sales/update-sales-leads/:lead_no",
-            element: <UpdateSalesLeads />,
-          },
-          /* Orders */
-          {
-            path: "sales-orders",
-            element: <SalesOrders />,
-          },
-          {
-            path: "sales-orders/add-sales-order",
-            element: <AddSalesOrder />,
-          },
+        element: <SalesDashboard />,
+      },
+      /* Leads */
+      {
+        path: "sales-leads",
+        element: <SalesLead />,
+      },
+      {
+        path: "sales/add-sales-leads",
+        element: <AddSalesLeads />,
+      },
+      {
+        path: "sales/update-sales-leads/:lead_no",
+        element: <UpdateSalesLeads />,
+      },
+      /* Orders */
+      {
+        path: "sales-orders",
+        element: <SalesOrders />,
+      },
+      {
+        path: "sales-orders/add-sales-order",
+        element: <AddSalesOrder />,
+      },
 
-          {
-            path: "sales-orders/update-sales-order/:order_id",
-            element: <UpdateSalesOrder />,
-          },
-          /* invoice */
-          {
-            path: "sales-invoices",
-            element: <SalesInvoices />,
-          },
-          {
-            path: "sales-invoices/sales-invoices-details/:invoice_id",
-            element: <SalesInvoiceDetails />,
-          },
-          /* ar */
-          {
-            path: "ar",
-            element: <AR />,
-          },
-          /* attendance */
-          {
-            path: "attendance",
-            element: <Attendance />,
-          },
-        ],
+      {
+        path: "sales-orders/update-sales-order/:order_id",
+        element: <UpdateSalesOrder />,
+      },
+      /* invoice */
+      {
+        path: "sales-invoices",
+        element: <SalesInvoices />,
+      },
+      {
+        path: "sales-invoices/sales-invoices-details/:invoice_id",
+        element: <SalesInvoiceDetails />,
+      },
+      /* ar */
+      {
+        path: "ar",
+        element: <AR />,
+      },
+      /* attendance */
+      {
+        path: "attendance",
+        element: <Attendance />,
       },
     ],
   },
